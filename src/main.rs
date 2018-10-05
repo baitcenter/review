8bcc9de49bf3a8fd7be2681ad866f8630ba3bd5f
Min Sik Kim <msk@dolbo.net>
2018-10-05 10:01:09 -0700

    Take an input file name on the command line

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate clap;
 
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("REview")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Petabi, Inc.")
-        .get_matches();
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true),
+        ).get_matches();
+    println!("Loading JSON file: {}", matches.value_of("INPUT").unwrap());
 }
