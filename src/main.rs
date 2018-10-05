bfcc4e9afb46a34a1a38b6fae175f426cd092017
Min Sik Kim <msk@dolbo.net>
2018-10-05 15:21:09 -0700

    Read clusters from a JSON file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 extern crate clap;
 
+#[macro_use]
+extern crate serde_derive;
+
+mod cluster;
+
 use clap::{App, Arg};
+use cluster::read_clusters_from_file;
 
 fn main() {
     let matches = App::new("REview")
@@ -11,5 +17,9 @@ fn main() {
                 .help("Sets the input file to use")
                 .required(true),
         ).get_matches();
-    println!("Loading JSON file: {}", matches.value_of("INPUT").unwrap());
+    let filename = matches.value_of("INPUT").unwrap();
+    match read_clusters_from_file(filename) {
+        Ok(clusters) => println!("read {} clusters from {}.", clusters.len(), filename),
+        Err(_) => println!("couldn't read the JSON file: {}", filename),
+    }
 }
