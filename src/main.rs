c0ab72eace29992ba2c23bc8047bbd4e9ab9c6e6
Min Sik Kim <msk@dolbo.net>
2018-10-05 09:48:42 -0700

    Use clap for command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate clap;
+
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("REview")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Petabi, Inc.")
+        .get_matches();
 }
