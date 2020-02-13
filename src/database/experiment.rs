04a83cbcf2f24feacf2a6ccfb246e8ac26c5264d
Sehkone Kim <sehkone@petabi.com>
2020-02-13 17:19:17 +0900

    linear regression of time series

--- src/database/experiment.rs
+++ src/database/experiment.rs
@@ -6,8 +6,10 @@ use actix_web::{
 use bigdecimal::{BigDecimal, FromPrimitive};
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
+use itertools::izip;
 use num_traits::ToPrimitive;
 use serde::{Deserialize, Serialize};
+use statistical::*;
 use std::collections::HashMap;
 
 use super::schema::{cluster, column_description, data_source, top_n_datetime};
@@ -29,8 +31,23 @@ pub(crate) struct ClusterSelectQuery {
     cluster_id: String,
 }
 
+#[derive(Debug, Deserialize)]
+pub(crate) struct DataSourceSelectQuery {
+    data_source: String,
+}
+
 #[derive(Debug, Queryable)]
-struct TopNDatetimeLoad {
+struct TopNDateTimeLoadOfCluster {
+    pub column_index: i32,
+    pub description_id: i32,
+    pub ranking: Option<i64>,
+    pub value: Option<NaiveDateTime>,
+    pub count: Option<i64>,
+}
+
+#[derive(Debug, Queryable)]
+struct TopNDateTimeLoadOfDataSource {
+    pub cluster_id: Option<String>,
     pub column_index: i32,
     pub description_id: i32,
     pub ranking: Option<i64>,
@@ -42,6 +59,21 @@ struct TopNDatetimeLoad {
 struct TimeSeriesTransfer {
     pub column_index: usize,
     pub series: Vec<(NaiveDateTime, usize)>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct TimeSeriesLinearRegression {
+    pub cluster_id: String,
+    pub series: Vec<(NaiveDateTime, usize)>,
+    pub slope: Option<f64>,
+    pub intercept: Option<f64>,
+    pub r_square: Option<f64>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct TopNTimeSeriesTransfer {
+    pub column_index: usize,
+    pub top_n: Vec<TimeSeriesLinearRegression>,
 }
 
 pub(crate) async fn get_sum_of_cluster_sizes(
@@ -85,7 +117,7 @@ pub(crate) async fn get_cluster_time_series(
     use column_description::dsl as col_d;
     use data_source::dsl as d_d;
     use top_n_datetime::dsl as top_d;
-    let query_result: Result<Vec<TopNDatetimeLoad>, database::Error> =
+    let query_result: Result<Vec<TopNDateTimeLoadOfCluster>, database::Error> =
         pool.get().map_err(Into::into).and_then(|conn| {
             c_d::cluster
                 .inner_join(d_d::data_source.on(c_d::data_source_id.eq(d_d::id)))
@@ -103,7 +135,7 @@ pub(crate) async fn get_cluster_time_series(
                         .eq(&query.data_source)
                         .and(c_d::cluster_id.eq(&query.cluster_id)),
                 )
-                .load::<TopNDatetimeLoad>(&conn)
+                .load::<TopNDateTimeLoadOfCluster>(&conn)
                 .map_err(Into::into)
         });
 
@@ -145,5 +177,102 @@ pub(crate) async fn get_cluster_time_series(
         Err(e) => Ok(HttpResponse::InternalServerError()
             .header(http::header::CONTENT_TYPE, "application/json")
             .body(build_err_msg(&e))),
+    }
+}
+
+pub(crate) async fn get_top_n_of_cluster_time_series_by_linear_regression(
+    pool: Data<database::Pool>,
+    query: Query<DataSourceSelectQuery>,
+) -> Result<HttpResponse, actix_web::Error> {
+    use cluster::dsl as c_d;
+    use column_description::dsl as col_d;
+    use data_source::dsl as d_d;
+    use top_n_datetime::dsl as top_d;
+    let query_result: Result<Vec<TopNDateTimeLoadOfDataSource>, database::Error> =
+        pool.get().map_err(Into::into).and_then(|conn| {
+            c_d::cluster
+                .inner_join(d_d::data_source.on(c_d::data_source_id.eq(d_d::id)))
+                .inner_join(col_d::column_description.on(col_d::cluster_id.eq(c_d::id)))
+                .inner_join(top_d::top_n_datetime.on(top_d::description_id.eq(col_d::id)))
+                .select((
+                    c_d::cluster_id,
+                    col_d::column_index,
+                    col_d::id,
+                    top_d::ranking,
+                    top_d::value,
+                    top_d::count,
+                ))
+                .filter(d_d::topic_name.eq(&query.data_source))
+                .load::<TopNDateTimeLoadOfDataSource>(&conn)
+                .map_err(Into::into)
+        });
+
+    match query_result {
+        Ok(values) => {
+            let mut series: HashMap<usize, HashMap<String, HashMap<NaiveDateTime, usize>>> = HashMap::new(); // TODO: BigDecimal later?
+
+            for v in values {
+                if let (Some(column_index), Some(cluster_id), Some(value), Some(count)) = (
+                    v.column_index.to_usize(),
+                    v.cluster_id,
+                    v.value,
+                    v.count.and_then(|c| c.to_usize()),
+                ) {
+                    *series
+                        .entry(column_index)
+                        .or_insert_with(HashMap::<String, HashMap<NaiveDateTime, usize>>::new)
+                        .entry(cluster_id)
+                        .or_insert_with(HashMap::<NaiveDateTime, usize>::new)
+                        .entry(value)
+                        .or_insert(0_usize) += count;
+                }
+            }
+
+            let mut series: Vec<TopNTimeSeriesTransfer> = series
+                .iter()
+                .map(|(column_index, cluster_series)|
+                    TopNTimeSeriesTransfer {
+                        column_index: *column_index,
+                        top_n: cluster_series.iter().map(|(cluster_id, series)| {
+                            let y_values: Vec<usize> = series.iter().map(|(_, count)| *count).collect();
+                            let (slope, intercept, r_square) = linear_regression(&y_values);
+                            TimeSeriesLinearRegression {
+                                cluster_id: cluster_id.clone(),
+                                series: series.iter().map(|(dt, count)| (*dt, *count)).collect(),
+                                slope,
+                                intercept,
+                                r_square,
+                            }
+                        }).collect(),
+                    }
+                ).collect();
+
+            Ok(HttpResponse::Ok()
+                .header(http::header::CONTENT_TYPE, "application/json")
+                .json(series))
+        }
+        Err(e) => Ok(HttpResponse::InternalServerError()
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .body(build_err_msg(&e))),
+    }
+}
+
+fn linear_regression(y_values: &[usize]) -> (Option<f64>, Option<f64>, Option<f64>) {
+    let x_values: Vec<f64> = (0..y_values.len()).map(|x| x.to_f64().expect("safe: usize -> f64")).collect();
+    let y_values: Vec<f64> = y_values.iter().map(|y| y.to_f64().expect("safe: usize -> f64")).collect();
+    if let Ok((slope, intercept)) = linreg::linear_regression::<f64, f64, f64>(&x_values, &y_values) {
+        let mut rss: f64 = 0.;
+        for (x, y) in izip!(x_values.iter(), y_values.iter()) {
+            rss += (y - (intercept + slope * x)).powi(2);
+        }
+        let y_mean = mean(&y_values);
+        let mut tss: f64 = 0.;
+        for y in y_values.iter() {
+            tss += (*y - y_mean).powi(2);
+        }
+        let r_square = 1. - rss / tss;
+        (Some(slope), Some(intercept), Some(r_square))
+    } else {
+        (None, None, None)
     }
 }
